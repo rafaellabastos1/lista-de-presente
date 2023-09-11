9d1fd658b21d07004ceec65b7e324b140b8feb4e
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-11 07:39:42 -0300

    mudei os números do click

--- js/paginas.js
+++ js/paginas.js
@@ -8,18 +8,12 @@ function redirecionar(numeroItem) {
         itens[numeroItem].innerHTML = '<a href = "./itens/livro.html">Redirecionando para livros</a>'
         window.location.href = "./itens/livro.html"
     } else if (numeroItem == 2) {
-        itens[numeroItem].innerHTML = '<a href = "./itens/beleza.html">Redirecionando para produtos de beleza</a>'
-        window.location.href = "./itens/beleza.html"
-    } else if (numeroItem == 3) {
         itens[numeroItem].innerHTML = '<a href = "./itens/decoracao.html">Redirecionando para decoração</a>'
         window.location.href = "./itens/decoracao.html"
-    } else if (numeroItem == 4) {
-        itens[numeroItem].innerHTML = '<a href = "./itens/magia.html">Redirecionando para magia</a>'
-        window.location.href = "./itens/magia.html"
-    } else if (numeroItem == 5) {
+    } else if (numeroItem == 3) {
         itens[numeroItem].innerHTML = '<a href = "./itens/ideias.html">Redirecionando para ideias</a>'
         window.location.href = "./itens/ideias.html"
-    } else if (numeroItem == 6) {
+    } else if (numeroItem == 4) {
         itens[numeroItem].innerHTML = '<a href = "./itens/pix.html">Redirecionando para número do pix</a>'
         window.location.href = "./itens/pix.html"
     } 
